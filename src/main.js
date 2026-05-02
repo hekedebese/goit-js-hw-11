c8564ceeead0af65238f2de375fed173da95497a
Nikita Anohin <anohinnikita537@gmail.com>
2026-05-02 17:15:16 +0300

    submit func fix

--- src/main.js
+++ src/main.js
@@ -16,24 +16,35 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  const value = event.currentTarget.elements['search-text'].value;
+  const value = event.currentTarget.elements['search-text'].value.trim();
+
+  if (!value) {
+    iziToast.error({
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
 
   clearGallery();
-
   showLoader();
 
   getImagesByQuery(value)
     .then(({ hits }) => {
-      console.log(hits);
       if (hits.length === 0) {
         iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
         });
+        return;
       }
+
       createGallery(hits);
     })
     .catch(error => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later!',
+      });
+
       console.log(error.message);
     })
     .finally(() => {
